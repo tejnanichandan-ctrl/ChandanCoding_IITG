5e47394bb9608394829d410f46ef63ef6135bf5b
Chandan <tejnanichandan@gmail.com>
2026-02-05 10:19:08 +0530

    Initial commit - Python lab files

---
+++ Area.py
@@ -0,0 +1,16 @@
+"""  # Script to calculate the are of a rectangle
+length = float(input("Enter the length of the rectangle: ")) # accepting length input from user
+breadth = float(input("Enter the breadth of the rectangle: ")) # accepting breadth input from user
+area = length * breadth 
+print("Length:", length) #accessing length variable
+
+print("Breadth:", breadth) #accessing breadth variable
+
+print("Area of Rectangle is:", area) #accessing area variable
+
+# Formula: Area = length * breadth
+# Example: If length = 4 and breadth = 5, then Area = 4 * 5 = 20
+# You can change the values of length and breadth to calculate area for different rectangles2  """
+
+x= 9//5
+print("Value :", x)
---
+++ example.py
@@ -0,0 +1,10 @@
+#print(5 % True)
+# print("Value :", 9//5)
+#print(-True**False)
+
+value_1 = (bool(1) == True)
+value_2 = (bool(0.1) == True)
+value_3 = (bool(-1) == True)
+value_4 = (bool("") == False)
+
+print(value_1, value_2, value_3, value_4)
